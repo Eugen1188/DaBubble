619d75f632e7e317e5a049eed5090638b5e94cea
Ioannis Karakasidis <168634993+Ioannis-Karakasidis@users.noreply.github.com>
2025-02-26 18:35:16 +0100

    Revert "."
    This reverts commit 6694b6d084a1f949f430489e53715a29697770b5.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideFirebaseApp(() => initializeApp({ projectId: "da-bubble-6b499", appId: "1:541571707880:web:1034b667b0fef0dfbbea5d", storageBucket: "da-bubble-6b499.firebasestorage.app", apiKey: "AIzaSyDyHgOhSccqviA3sW5tEU7ZB208mG2JBUY", authDomain: "da-bubble-6b499.firebaseapp.com", messagingSenderId: "541571707880" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
 };
