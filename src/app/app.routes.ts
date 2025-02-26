7f7c24b2a0cbb861890c5ddbe75293323fdbead6
Marc Schaar <m-schaar@gmx.net>
2025-02-26 19:59:17 +0100

    add chat rout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { IntroComponent } from './intro/intro.component';
 import { MaincomponentComponent } from './maincomponent/maincomponent.component';
+import { MainChatComponent } from './main-chat/main-chat.component';
 
 export const routes: Routes = [
   { path: '', component: IntroComponent },
   { path: 'main', component: MaincomponentComponent },
+  { path: 'chat', component: MainChatComponent },
 ];
