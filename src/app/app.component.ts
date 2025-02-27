8a7f81a072d82f5e61f3a045dd1eab9a78ca2054
Eugen1188 <eugen.ferchow@gmx.de>
2025-02-27 18:57:17 +0100

    profile_update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'DaBubble';
+  title = 'Portfolio';
+
 }
