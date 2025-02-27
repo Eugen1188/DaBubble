65d4927590ad7207f62775013e9f283237113997
Marc Schaar <m-schaar@gmx.net>
2025-02-27 09:16:56 +0100

    add tdate seperaor

--- src/app/main-chat/main-chat.component.ts
+++ src/app/main-chat/main-chat.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-main-chat',
-  imports: [MatIconModule, MatButtonModule],
+  imports: [MatIconModule, MatButtonModule, MatDividerModule],
   templateUrl: './main-chat.component.html',
   styleUrl: './main-chat.component.scss',
 })
