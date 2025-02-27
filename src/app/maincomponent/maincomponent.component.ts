3246a150046ef8457a76e1425e8fd50f99566caf
Ioannis Karakasidis <168634993+Ioannis-Karakasidis@users.noreply.github.com>
2025-02-27 03:57:04 +0100

    .

--- src/app/maincomponent/maincomponent.component.ts
+++ src/app/maincomponent/maincomponent.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { SigninComponent } from '../signin/signin.component';
 
 @Component({
   selector: 'app-maincomponent',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, SigninComponent],
   templateUrl: './maincomponent.component.html',
   styleUrl: './maincomponent.component.scss',
 })
--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-signin',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './signin.component.html',
-  styleUrl: './signin.component.scss'
+  styleUrl: './signin.component.scss',
 })
-export class SigninComponent {
-
-}
+export class SigninComponent {}
