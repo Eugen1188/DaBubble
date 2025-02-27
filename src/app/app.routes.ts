0a411f81cba03e7f20ad725f76b123c6b1adc0f4
Eugen1188 <eugen.ferchow@gmx.de>
2025-02-27 20:54:11 +0100

    update_profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { IntroComponent } from './intro/intro.component';
 import { MaincomponentComponent } from './maincomponent/maincomponent.component';
 import { MainChatComponent } from './main-chat/main-chat.component';
 import { SigninComponent } from './signin/signin.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 export const routes: Routes = [
   { path: '', component: IntroComponent },
   { path: 'main', component: MaincomponentComponent },
   { path: 'signin', component: SigninComponent },
   { path: 'chat', component: MainChatComponent },
+  { path: 'user-profile', component: UserProfileComponent },
+
 ];
