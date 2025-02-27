95d430506c8f9505fd9b01ec0c806c475ca1eddd
Ioannis Karakasidis <168634993+Ioannis-Karakasidis@users.noreply.github.com>
2025-02-27 21:41:46 +0100

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,22 +3,13 @@ import { IntroComponent } from './intro/intro.component';
 import { MaincomponentComponent } from './maincomponent/maincomponent.component';
 import { MainChatComponent } from './main-chat/main-chat.component';
 import { SigninComponent } from './signin/signin.component';
-<<<<<<< Updated upstream
-import { UserProfileComponent } from './user-profile/user-profile.component';
-=======
->>>>>>> Stashed changes
 import { SignupComponent } from './signup/signup.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 export const routes: Routes = [
-  { path: '', component: IntroComponent },
   { path: 'main', component: MaincomponentComponent },
   { path: 'signin', component: SigninComponent },
   { path: 'chat', component: MainChatComponent },
-<<<<<<< Updated upstream
   { path: 'user-profile', component: UserProfileComponent },
   { path: 'singup', component: SignupComponent },
-=======
-  { path: 'singup', component: SignupComponent },
-
->>>>>>> Stashed changes
 ];
