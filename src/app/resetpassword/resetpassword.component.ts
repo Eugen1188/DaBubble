2e7f306fa8003811356a855f5c810359b467ee00
Ioannis Karakasidis <168634993+Ioannis-Karakasidis@users.noreply.github.com>
2025-02-28 07:51:53 +0100

    .

--- src/app/resetpassword/resetpassword.component.ts
+++ src/app/resetpassword/resetpassword.component.ts
@@ -1,18 +1,36 @@
 import { Component } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { User } from '../../models/user.class';
 import { CommonModule } from '@angular/common';
+import { getAuth, updatePassword } from 'firebase/auth';
 
 @Component({
   selector: 'app-resetpassword',
   standalone: true,
   imports: [HeaderComponent, FooterComponent, FormsModule, CommonModule],
   templateUrl: './resetpassword.component.html',
-  styleUrl: './resetpassword.component.scss',
+  styleUrls: ['./resetpassword.component.scss'],
 })
 export class ResetpasswordComponent {
-  user = new User();
-  onSubmit() {}
+  users = new User();
+  auth = getAuth();
+  currentUser = this.auth.currentUser;
+  newPassword = this.users.password;
+
+  async onSubmit(emailform: NgForm) {
+    if (this.currentUser) {
+      try {
+        await updatePassword(this.currentUser, this.newPassword);
+        console.log('Password changed successfully');
+        emailform.reset();
+      } catch (error) {
+        console.error('Error changing password:', error);
+      }
+    } else {
+      console.error('No user is currently signed in');
+    }
+  }
 }
