dc5aa516ea309edcd00902eb4104388502763915
Marc Schaar <m-schaar@gmx.net>
2025-03-01 16:08:52 +0100

    add scroll bottom

--- src/app/main-chat/main-chat.component.ts
+++ src/app/main-chat/main-chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { AfterViewInit, Component, inject, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
@@ -10,9 +10,34 @@ import { ShareddataService } from '../shareddata.service';
   templateUrl: './main-chat.component.html',
   styleUrl: './main-chat.component.scss',
 })
-export class MainChatComponent {
+export class MainChatComponent implements OnInit, AfterViewInit {
   chatmodule = inject(ShareddataService);
+
   constructor() {
     this.chatmodule.chatmoduleenabled = true;
+  }
+
+  ngOnInit(): void {
+    console.log('init');
+    this.scrollToBottom();
+  }
+
+  ngAfterViewInit(): void {
+    const chatContent = document.querySelector('.chat-content');
+    if (chatContent) {
+      const observer = new MutationObserver(() => this.scrollToBottom());
+      observer.observe(chatContent, { childList: true, subtree: true });
+    }
+  }
+
+  scrollToBottom(): void {
+    setTimeout(() => {
+      const chatContent = document.querySelector(
+        '.chat-content'
+      ) as HTMLElement;
+      if (chatContent) {
+        chatContent.scrollTop = chatContent.scrollHeight;
+      }
+    }, 1000);
   }
 }
