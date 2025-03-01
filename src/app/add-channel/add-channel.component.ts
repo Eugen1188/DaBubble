80b460c9453d82c195953e06802655d188c8620f
Eugen1188 <eugen.ferchow@gmx.de>
2025-03-01 21:31:01 +0100

    update_add_channels

--- src/app/add-channel/add-channel.component.ts
+++ src/app/add-channel/add-channel.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-add-channel',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-channel.component.html',
   styleUrl: './add-channel.component.scss'
 })
 export class AddChannelComponent {
+
+  channelName: string = "";
+  channelDescription: string = "";
+
+  closeScree() {
+
+  }
+
+  onSubmit() {
+    console.log("submit");
+  }
 
 }
