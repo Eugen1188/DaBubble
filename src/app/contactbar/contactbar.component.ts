82d819ddc0ff783cf6a926cd1986ad612a867b81
Johannes Roth <jrothx@icloud.com>
2025-03-01 16:50:35 +0100

    create contactbar

---
+++ src/app/contactbar/contactbar.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contactbar',
+  imports: [CommonModule],
+  templateUrl: './contactbar.component.html',
+  styleUrl: './contactbar.component.scss'
+})
+export class ContactbarComponent {
+
+}
