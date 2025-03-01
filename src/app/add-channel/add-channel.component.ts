6ca99dfa8acad2286fc2bdc80da8dfe8cc88c025
Eugen1188 <eugen.ferchow@gmx.de>
2025-03-02 00:45:08 +0100

    add_aos_animation

--- src/app/add-channel/add-channel.component.ts
+++ src/app/add-channel/add-channel.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
+import { FormsModule } from '@angular/forms';
+import * as AOS from 'aos';
+import 'aos/dist/aos.css';
 
 @Component({
   selector: 'app-add-channel',
@@ -12,6 +15,15 @@ export class AddChannelComponent {
 
   channelName: string = "";
   channelDescription: string = "";
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init();
+    }
+  }
 
   closeScree() {
 
@@ -34,11 +46,13 @@ export class AddChannelComponent {
   expandTextarea(event: Event) {
     const textarea = event.target as HTMLTextAreaElement;
     textarea.style.height = '90px';
+    if(textarea.scrollHeight > 90) {
+      textarea.style.height =`${textarea.scrollHeight}px`;
+    }
   }
 
   shrinkTextarea(event: Event) {
     const textarea = event.target as HTMLTextAreaElement;
     textarea.style.height = '60px';
   }
-
 }
