67073da85027a5bd9b7c0ddfafb68d2811b87ccc
Johannes Roth <jrothx@icloud.com>
2025-03-04 11:30:30 +0100

    contact bar completed

--- src/app/contactbar/contactbar.component.ts
+++ src/app/contactbar/contactbar.component.ts
@@ -8,5 +8,64 @@ import { Component } from '@angular/core';
   styleUrl: './contactbar.component.scss'
 })
 export class ContactbarComponent {
+  active: boolean = false;
+  message: boolean = false;
+  channels = [
+    {
+      name: 'Entwicklerteam'
+    },
+    {
+      name: 'Backoffice'
+    },
+    {
+      name: 'Support'
+    },
 
+  ]
+
+  contacts=[
+    {
+      name:'Frederik Beck (Du)',
+      default:'/img/man2active.svg'
+    },
+    {
+      name:'Sofia Müller',
+      default:'/img/woman1.svg'
+    },
+    {
+      name:'Noah Braun',
+      default:'/img/man1active.svg'
+    },
+    {
+      name:'Elise Roth',
+      default:'/img/woman2.svg'
+    },
+    {
+      name:'Elias Neumann',
+      default:'/img/man3active.svg'
+    },
+    {
+      name:'Steffen Hoffmann',
+      default:'/img/man4active.svg'
+    },
+  ]
+
+  toggleActive() {
+    this.active = !this.active;
+    console.log(this.active);
+  }
+
+  toggleMessage() {
+    this.message = !this.message;
+    console.log('message = ' + this.message);
+
+  }
+
+  isOpen() {
+    return this.message === true;
+  }
+
+  isActive() {
+    return this.active === true;
+  }
 }
