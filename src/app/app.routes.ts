b0f598c414a33e527afadc3ae2030a17b214dffc
SaschaBorowski <saschaborowski@outlook.de>
2025-03-08 15:45:17 +0100

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { IntroComponent } from './intro/intro.component';
 import { MaincomponentComponent } from './maincomponent/maincomponent.component';
 import { MainChatComponent } from './main-chat/main-chat.component';
 import { SigninComponent } from './signin/signin.component';
+import { ThreadComponent } from './thread/thread.component';
 
 export const routes: Routes = [
   { path: '', component: IntroComponent },
   { path: 'main', component: MaincomponentComponent },
   { path: 'signin', component: SigninComponent },
   { path: 'chat', component: MainChatComponent },
+  { path: 'thread', component: ThreadComponent },
 ];
