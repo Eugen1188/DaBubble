7db4f435e7871a217aaf26fd755dcb2ceab8b107
Marc Schaar <m-schaar@gmx.net>
2025-03-08 23:20:27 +0100

    open tread placeholder by click on 2 Antworten

--- src/app/chat-content/chat-content.component.ts
+++ src/app/chat-content/chat-content.component.ts
@@ -5,18 +5,27 @@ import {
   inject,
   OnInit,
   ViewChild,
+  Input,
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Message } from '../../models/message.class';
-import { User } from '../../models/user.class';
 import { onSnapshot, updateDoc, arrayUnion } from '@angular/fire/firestore';
 import { FireServiceService } from '../fire-service.service';
 import { UserService } from '../shared.service';
+import { Output, EventEmitter } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 @Component({
   selector: 'app-chat-content',
-  imports: [MatIconModule, MatButtonModule, CommonModule, FormsModule],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    CommonModule,
+    FormsModule,
+    MatSidenavModule,
+  ],
   templateUrl: './chat-content.component.html',
   styleUrl: './chat-content.component.scss',
 })
@@ -257,5 +266,11 @@ export class ChatContentComponent implements OnInit {
 
   editMessage(m: any, i: number) {
     console.log(m, i);
+  }
+
+  @Output() toggleThread = new EventEmitter<void>();
+
+  toggle() {
+    this.toggleThread.emit();
   }
 }
--- src/app/main-chat/main-chat.component.ts
+++ src/app/main-chat/main-chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
@@ -7,7 +7,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from '../header/header.component';
 import { ChatContentComponent } from '../chat-content/chat-content.component';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-main-chat',
@@ -26,10 +26,15 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 })
 export class MainChatComponent {
   chatmodule = inject(UserService);
+  @ViewChild('drawer') drawer!: MatDrawer;
   showFiller = true;
 
   constructor() {
     this.chatmodule.chatmoduleenabled = true;
     this.chatmodule.accountcreation = false;
+  }
+
+  toggleThread() {
+    this.drawer.toggle(); // MatDrawer direkt toggeln
   }
 }
