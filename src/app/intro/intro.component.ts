75ec9533b9d22e9b9f7d57f44a5ffc23d21bf7b1
Ioannis Karakasidis <168634993+Ioannis-Karakasidis@users.noreply.github.com>
2025-03-08 17:32:42 +0100

    .

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,10 +1,3 @@
-import {
-  animate,
-  state,
-  style,
-  transition,
-  trigger,
-} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -12,27 +5,6 @@ import { Router } from '@angular/router';
   selector: 'app-intro',
   templateUrl: './intro.component.html',
   styleUrls: ['./intro.component.scss'],
-  animations: [
-    trigger('moveUp', [
-      state('beginup', style({ transform: 'translate(0, 0)' })),
-      state(
-        'endbegin',
-        style({
-          transform: 'translate(-947px, -565px)',
-          width: '70px',
-          height: '75px',
-        })
-      ),
-      transition('beginup => endbegin', animate('1500ms  ease-out')),
-    ]),
-    trigger('moveRight', [
-      state('startright', style({ transform: 'translateX(0)' })),
-      state('end', style({ transform: 'translateX(200px)' })),
-      state('back', style({ transform: 'translateX(93px)' })),
-      transition('startright => end', animate('1000ms ease-out')),
-      transition('end => back', animate('1000ms 500ms ease-out')),
-    ]),
-  ],
 })
 export class IntroComponent implements OnInit {
   animationStateUp = 'beginup';
@@ -42,16 +14,7 @@ export class IntroComponent implements OnInit {
 
   ngOnInit(): void {
     setTimeout(() => {
-      this.animationStateRight = 'end';
-      setTimeout(() => {
-        setTimeout(() => {
-          this.animationStateRight = 'back';
-        }, 200);
-        this.animationStateUp = 'endbegin';
-      }, 3800);
-      setTimeout(() => {
-       // this.router.navigate(['/main']);
-      }, 6500);
+      this.router.navigate(['/main']);
     }, 2500);
   }
 }
