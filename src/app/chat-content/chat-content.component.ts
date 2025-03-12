65b2ed82dfc5f7cc6838894f739349ba2fd79710
Marc Schaar <m-schaar@gmx.net>
2025-03-12 19:14:55 +0100

    show current channel name

--- src/app/chat-content/chat-content.component.ts
+++ src/app/chat-content/chat-content.component.ts
@@ -5,7 +5,6 @@ import {
   inject,
   OnInit,
   ViewChild,
-  Input,
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
@@ -42,141 +41,6 @@ export class ChatContentComponent implements OnInit {
   currentChannel: any;
   input: string = '';
 
-  dummyData = [
-    {
-      name: 'Alice',
-      date: '2025-03-01',
-      time: '14:30',
-      message: 'Hallo, wie geht’s dir?',
-      newDay: true,
-      avatar: '/img/avatars/avatar_3.png',
-    },
-    {
-      name: 'Bob',
-      date: '2025-03-01',
-      time: '14:31',
-      message: 'Mir geht’s gut, danke! Und dir?',
-      newDay: false,
-      avatar: '/img/avatars/avatar_1.png',
-    },
-    {
-      name: 'Alice',
-      date: '2025-03-01',
-      time: '14:32',
-      message: 'Auch gut, danke der Nachfrage!',
-      newDay: false,
-      avatar: '/img/avatars/avatar_3.png',
-    },
-    {
-      name: 'Bob',
-      date: '2025-03-01',
-      time: '14:33',
-      message: 'Was hast du heute so vor?',
-      newDay: false,
-      avatar: '/img/avatars/avatar_1.png',
-    },
-    {
-      name: 'Alice',
-      date: '2025-03-01',
-      time: '14:35',
-      message: 'Ich wollte ein bisschen lesen und später spazieren gehen.',
-      newDay: false,
-      avatar: '/img/avatars/avatar_3.png',
-    },
-    {
-      name: 'Bob',
-      date: '2025-03-01',
-      time: '14:36',
-      message: 'Klingt gut! Welches Buch liest du gerade?',
-      newDay: false,
-      avatar: '/img/avatars/avatar_1.png',
-    },
-    {
-      name: 'Alice',
-      date: '2025-03-01',
-      time: '14:37',
-      message: 'Einen Krimi von Agatha Christie. Richtig spannend!',
-      newDay: false,
-      avatar: '/img/avatars/avatar_3.png',
-    },
-    {
-      name: 'Bob',
-      date: '2025-03-01',
-      time: '14:38',
-      message: 'Oh cool! Ich mag ihre Bücher auch.',
-      newDay: false,
-      avatar: '/img/avatars/avatar_1.png',
-    },
-
-    {
-      name: 'Alice',
-      date: '2025-03-02',
-      time: '10:15',
-      message: 'Guten Morgen! Wie hast du geschlafen?',
-      newDay: true,
-      avatar: '/img/avatars/avatar_3.png',
-    },
-    {
-      name: 'Bob',
-      date: '2025-03-02',
-      time: '10:17',
-      message: 'Guten Morgen! Ganz gut, danke. Und du?',
-      newDay: false,
-      avatar: '/img/avatars/avatar_1.png',
-    },
-    {
-      name: 'Alice',
-      date: '2025-03-02',
-      time: '10:20',
-      message: 'Auch gut! Ich hab ein bisschen länger geschlafen.',
-      newDay: false,
-      avatar: '/img/avatars/avatar_3.png',
-    },
-    {
-      name: 'Bob',
-      date: '2025-03-02',
-      time: '10:22',
-      message: 'Manchmal braucht man das!',
-      newDay: false,
-      avatar: '/img/avatars/avatar_1.png',
-    },
-
-    {
-      name: 'Alice',
-      date: '2025-03-03',
-      time: '09:00',
-      message: 'Neuer Tag, neues Glück!',
-      newDay: true,
-      avatar: '/img/avatars/avatar_3.png',
-    },
-    {
-      name: 'Bob',
-      date: '2025-03-03',
-      time: '09:05',
-      message: 'Genau! Was steht heute an?',
-      newDay: false,
-      avatar: '/img/avatars/avatar_1.png',
-    },
-    {
-      name: 'Alice',
-      date: '2025-03-03',
-      time: '09:10',
-      message: 'Ich muss ein paar Dinge erledigen, aber danach habe ich Zeit.',
-      newDay: false,
-      avatar: '/img/avatars/avatar_3.png',
-    },
-    {
-      name: 'Bob',
-      date: '2025-03-03',
-      time: '09:12',
-      message: 'Lass uns später treffen!',
-      newDay: false,
-      avatar: '/img/avatars/avatar_1.png',
-    },
-  ];
-
-  testMode: boolean = false;
-
   unsubChannels!: () => void;
   unsubMessages!: () => void;
 
@@ -188,32 +52,30 @@ export class ChatContentComponent implements OnInit {
     this.channels = [];
     this.unsubChannels = onSnapshot(
       this.fireService.getCollectionRef('channels'),
-      (list) => {
-        this.channels = list.docs.map((element) => ({
-          key: element.id,
-          data: element.data(),
+      (colSnap) => {
+        this.channels = colSnap.docs.map((colSnap) => ({
+          key: colSnap.id,
+          data: colSnap.data(),
         }));
         this.currentChannel = this.channels[0];
+        console.log(this.currentChannel);
+
         this.getMessages();
       }
     );
   }
 
   getMessages() {
-    //this.messages = []; wegen dem nach untenscrollen auskommentiert
-
-    if (!this.testMode) {
-      this.unsubMessages = onSnapshot(
-        this.fireService.getDocRef('channels', this.currentChannel.key),
-        (element) => {
-          if (element.exists())
-            (this.messages = element
-              .data()
-              ['messages'].map((m: any) => new Message(m))),
-              this.scrollToBottom();
-        }
-      );
-    } else this.messages = this.dummyData.map((m: any) => new Message(m));
+    this.unsubMessages = onSnapshot(
+      this.fireService.getDocRef('channels', this.currentChannel.key),
+      (docSnap) => {
+        if (docSnap.exists())
+          (this.messages = docSnap
+            .data()
+            ['messages'].map((m: any) => new Message(m))),
+            this.scrollToBottom();
+      }
+    );
   }
 
   buildMessageObject(): {} {
