54e3ad450a47bf5ae31286090a925d2548486f10
Johannes Roth <jrothx@icloud.com>
2025-03-15 08:36:54 +0100

    optimize channel

--- src/app/chat-content/chat-content.component.ts
+++ src/app/chat-content/chat-content.component.ts
@@ -5,6 +5,7 @@ import {
   inject,
   OnInit,
   ViewChild,
+  Injectable
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
@@ -15,6 +16,11 @@ import { FireServiceService } from '../fire-service.service';
 import { UserService } from '../shared.service';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
 
 @Component({
   selector: 'app-chat-content',
@@ -30,7 +36,7 @@ import { Router } from '@angular/router';
 })
 export class ChatContentComponent implements OnInit {
   @ViewChild('chatContent') chatContentRef!: ElementRef;
-
+  private subscription?: Subscription;
   fireService: FireServiceService = inject(FireServiceService);
   userService: UserService = inject(UserService);
   router: Router = inject(Router);
@@ -39,7 +45,7 @@ export class ChatContentComponent implements OnInit {
   menuOpen: boolean = false;
   channels: any = [];
   currentChannel: any = {};
-
+  currentUser: any;
   unsubMessages!: () => void;
 
   messages: Message[] = [];
@@ -53,7 +59,38 @@ export class ChatContentComponent implements OnInit {
       this.currentChannel = this.channels[0];
       this.getMessages();
     }
+
+    this.subscription = this.userService.startLoadingChannel$.subscribe(() => {
+      this.startChannel();
+    });
+
+
   }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  startChannel() {
+    console.log('start');
+    if (this.userService.user != null && this.userService.currentChannel != null) {
+    this.setCurrentChannel();
+    console.log(this.currentChannel);
+    console.log(this.currentUser);
+    
+    }else{
+      console.log('keine User oder Channel');
+    }
+  }
+
+
+  setCurrentChannel() {
+    this.currentChannel = this.userService.currentChannel;
+    this.currentUser = this.userService.currentUser;
+  }
+
 
   getMessages(): void {
     let docRef = this.fireService.getDocRef('channels', this.currentChannel.id);
@@ -62,7 +99,7 @@ export class ChatContentComponent implements OnInit {
         if (docSnap.exists()) {
           this.messages = docSnap
             .data()
-            ['messages'].map((m: Message) => new Message(m));
+          ['messages'].map((m: Message) => new Message(m));
         } else return;
       });
     }
